45199423ce318a0bdd7c39fb312c80497f67a019
Ariel Huang <arielhuang@ariels-macbook-pro.local>
2019-04-14 13:31:36 -0400

    Set up connection to database. create table, date list for requesting data in construction

--- vis1.py
+++ vis1.py
@@ -5,6 +5,7 @@ import sqlite3
 import json
 import unittest
 import requests
+import datetime
 
 #For Lufthansa API
 flight_key="zejvcb5vfcub7kyb22jv5grm"
@@ -20,13 +21,24 @@ r_token=requests.post('https://api.lufthansa.com/v1/oauth/token',data={
 s_token=r_token.json()
 access_token=s_token['access_token']
 
+#Generating date stings
+#TO BE UPDATED
+date_lst=[]
+for i in range(1,32):
+    date=datetime.datetime(2019,1,i)
+    date_lst.append(date)
+
 #Connecting the database
-conn=sqlite3.connect('Lufthansa.sqlite')
-cur=conn.cursor()
+conn1=sqlite3.connect('Lufthansa.sqlite')
+cur1=conn1.cursor()
 
+#Setting up the table
+cur1.execute('DROP TABLE IF EXISTS Fares')
+cur1.execute('CREATE TABLE Fares (origin TEXT, destination TEXT, travel_date TIMESTAMP, cabin TEXT, adult_traveler INTEGER)')
 
-
-#Getting data from Lufthansa API
+#Requesting flight fare data from Lufthansa API
+#at least 100 rows?
+#the best way to limit number of requests each time?
 
 
 if __name__ == "__main__":

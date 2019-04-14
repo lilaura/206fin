b0a2f908a19ea6cce68fe1b753c7238b4db07fce
Ariel Huang <arielhuang@ariels-macbook-pro.local>
2019-04-14 13:12:17 -0400

    Set up database; connect to database

--- vis1.py
+++ vis1.py
@@ -10,7 +10,7 @@ import requests
 flight_key="zejvcb5vfcub7kyb22jv5grm"
 flight_secret="7MYH4x8aRS"
 
-#Gettig access token from Lufthansa
+#Getting access token from Lufthansa
 r_token=requests.post('https://api.lufthansa.com/v1/oauth/token',data={
                      'client_id':flight_key,
                      'client_secret':flight_secret,
@@ -19,6 +19,11 @@ r_token=requests.post('https://api.lufthansa.com/v1/oauth/token',data={
 
 s_token=r_token.json()
 access_token=s_token['access_token']
+
+#Connecting the database
+conn=sqlite3.connect('Lufthansa.sqlite')
+cur=conn.cursor()
+
 
 
 #Getting data from Lufthansa API

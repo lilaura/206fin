35ca6fd9af4ee24d3af3e687f505bf57710bc83a
Ariel Huang <arielhuang@0587365546.wireless.umich.net>
2019-04-16 16:49:02 -0400

    Add data to database; minor changes; see comments; requesting data from flight API done

--- vis1.py
+++ vis1.py
@@ -8,15 +8,15 @@ import requests
 import time
 
 #For Cirium Flight API
-flight_id="8ab51357"
-flight_key="92daab56f079dc523afab96f0a5ef06a"
+flightAPI_id="8ab51357"
+flightAPI_key="92daab56f079dc523afab96f0a5ef06a"
 
 #Connecting the database
 conn=sqlite3.connect('db_fin.sqlite')
 cur=conn.cursor()
 
 #Setting up the table
-cur.execute('CREATE TABLE IF NOT EXISTS Flights (year TEXT, month TEXT, day TEXT, hour TEXT, flight_id TEXT, scheduled_runway_dep TIMESTAMP, actual_runway_dep TIMESTAMP, scheduled_runway_arr TIMESTAMP, actual_runway_arr TIMESTAMP)')
+cur.execute('CREATE TABLE IF NOT EXISTS Flights (day TEXT, hour TEXT, flight_id TEXT, scheduled_runway_dep TIMESTAMP, actual_runway_dep TIMESTAMP, scheduled_runway_arr TIMESTAMP, actual_runway_arr TIMESTAMP)')
 
 #define read_cache and write_cache functions
 def read_cache(cache_file):
@@ -44,7 +44,7 @@ hours=hours*5
 
 #Requesting historical flight data from Cirium Flight API; departing from DTW, 120hrs starting from 19-04-01T00:00
 for i in range(120):
-    r=requests.get('https://api.flightstats.com/flex/flightstatus/historical/rest/v3/json/airport/status/DTW/dep/2019/4/{}/{}'.format(days[i],hours[i]),params={'appId':flight_id, 'appKey':flight_key, 'utc':False, 'numHours':'1', 'maxFlights':'20'})
+    r=requests.get('https://api.flightstats.com/flex/flightstatus/historical/rest/v3/json/airport/status/DTW/dep/2019/4/{}/{}'.format(days[i],hours[i]),params={'appId':flightAPI_id, 'appKey':flightAPI_key, 'utc':'False', 'numHours':'1', 'maxFlights':'20'})
     dict=r.json()
     
     #checking to see if dup data exists in database
@@ -57,11 +57,21 @@ for i in range(120):
         except:
             pass
 
+#variables to add to database; I captured UTC time just b/c it will be easier to calculate scheduled vs. actual air time; when requesting data it was still DTW local time used; we can change that tho, or just look at heathrow
+        day=dict['day']['requested']
+        hour=dict['hour']['requested']
+        flight_id=flight['flightId']
+        scheduled_runway_dep=flight['scheduledRunwayDeparture']['dateUtc']
+        actual_runway_dep=flight['actualRunwayDeparture']['dateUtc']
+        scheduled_runway_arr=flight['actualRunwayDeparture']['dateUtc']
+        actual_runway_arr=flight['actualRunwayDeparture']['dateUtc']
+
+        cur.execute('INSERT INTO Flights (day, hour, flight_id, scheduled_runway_dep, actual_runway_dep, scheduled_runway_arr, actual_runway_arr) VALUES (?,?,?,?,?,?,?)',(day,hour,flight_id,scheduled_runway_dep,actual_runway_dep,scheduled_runway_arr,actual_runway_arr))
+        conn.committ
 
 
-
-print("Pausing for a bit...")
-time.sleep(10 )
+    print("Pausing for a bit...")
+    time.sleep(10)
 
 
 if __name__ == "__main__":

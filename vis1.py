c9e693ee9c5c4cdc04c1ca9044874d3ef844b4ea
Ariel Huang <arielhuang@ariels-macbook-pro.local>
2019-04-17 20:52:20 -0400

    Changed to collecting airport data

--- vis1.py
+++ vis1.py
@@ -16,63 +16,37 @@ conn=sqlite3.connect('db_fin.sqlite')
 cur=conn.cursor()
 
 #Setting up the table
-cur.execute('CREATE TABLE IF NOT EXISTS Flights (day TEXT, hour TEXT, flight_id TEXT, scheduled_runway_dep TIMESTAMP, actual_runway_dep TIMESTAMP, scheduled_runway_arr TIMESTAMP, actual_runway_arr TIMESTAMP)')
+cur.execute('CREATE TABLE IF NOT EXISTS Airports (name TEXT, city TEXT, state TEXT, latitude REAL, longitude REAL, elevation REAL, active_status TEXT)')
 
-#define read_cache and write_cache functions
-def read_cache(cache_file):
-    fr=open(cache_file)
-    s=fr.read()
-    d=json.loads(s)
-    fr.close()
-    return d
+#Requesting airport data from Cirium Flight API
+r=requests.get('https://api.flightstats.com/flex/airports/rest/v1/json/countryCode/US', params={'appId':flightAPI_id, 'appKey':flightAPI_key})
+js=r.json()
+airport_lst=js['airports']
+fw=open("airport_data.json",'w')
+fw.write(fw)
+fw.close()
 
-def write_cache(cache_file,cache_d):
-    fw=open(cache_file,"w")
-    js=json.dumps(cache_d)
-    fw.write(js)
-    fw.close()
-
-#Create days and hours lists to plug in requests url
-days=[]
-for i in range(1,6):
-    for n in range(0,24):
-        days.append(str(i))
-hours=[]
-for i in range(0,24):
-    hours.append(str(i))
-hours=hours*5
-
-#Requesting historical flight data from Cirium Flight API; departing from DTW, 120hrs starting from 19-04-01T00:00
-for i in range(120):
-    r=requests.get('https://api.flightstats.com/flex/flightstatus/historical/rest/v3/json/airport/status/DTW/dep/2019/4/{}/{}'.format(days[i],hours[i]),params={'appId':flightAPI_id, 'appKey':flightAPI_key, 'utc':'False', 'numHours':'1', 'maxFlights':'20'})
-    dict=r.json()
+#Adding items to database
+count=0
+for airport in airport_lst:
+    if count>20:
+        print("Added 20 items to database; restart to add more")
+        break
+    
     
     #checking to see if dup data exists in database
-    for flight in dict['flightStatuses']:
-        cur.execute('SELECT * FROM Flights WHERE flight_id=?',flight['flightId'])
-        try:
-            data=cur.fetchone()[0]
-            print ("Found in database")
-            continue
-        except:
-            pass
-
-#variables to add to database; I captured UTC time just b/c it will be easier to calculate scheduled vs. actual air time; when requesting data it was still DTW local time used; we can change that tho, or just look at heathrow
-        day=dict['day']['requested']
-        hour=dict['hour']['requested']
-        flight_id=flight['flightId']
-        scheduled_runway_dep=flight['scheduledRunwayDeparture']['dateUtc']
-        actual_runway_dep=flight['actualRunwayDeparture']['dateUtc']
-        scheduled_runway_arr=flight['actualRunwayDeparture']['dateUtc']
-        actual_runway_arr=flight['actualRunwayDeparture']['dateUtc']
-
-        cur.execute('INSERT INTO Flights (day, hour, flight_id, scheduled_runway_dep, actual_runway_dep, scheduled_runway_arr, actual_runway_arr) VALUES (?,?,?,?,?,?,?)',(day,hour,flight_id,scheduled_runway_dep,actual_runway_dep,scheduled_runway_arr,actual_runway_arr))
-        conn.committ
+    cur.execute('SELECT * FROM Airports WHERE name=?',airport['name'])
+    try:
+        data=cur.fetchone()[0]
+        print ("Found in database")
+        continue
+    except:
+        pass
 
 
-    print("Pausing for a bit...")
-    time.sleep(10)
-
+    cur.execute('INSERT INTO Flights (name, city, state, latitude, longitude, elevation, active_status) VALUES (?,?,?,?,?,?,?)',(airport['name'],airport['city'],airport['stateCode'],airport['latitude'],airport['longitude'],airport['elevation'],airport['active']))
+    conn.commit()
+    count+=1
 
 if __name__ == "__main__":
     

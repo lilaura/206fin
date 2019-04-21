f794d4327fc1dad3ff9268d414974a99b037bfdb
lilaura <lilaura@umich.edu>
2019-04-21 17:33:20 -0400

    cache

--- vis2.py
+++ vis2.py
@@ -24,7 +24,6 @@ def calc_avg_elev(conn, cur):
         avg_elev[key]=sum(avg_elev[key])/len(avg_elev[key])
     return avg_elev
 
-<<<<<<< HEAD
 def avg_temp(conn,cur):
     d = {}
     templist = []
@@ -39,17 +38,19 @@ def avg_temp(conn,cur):
         preciplist.append(row[4])
         vislist.append(row[6])
         latlist.append(row[2])
+        windlist.append(row[5])
+        lnglist.append(row[3])
     d["longitude"] = lnglist
     d["latitude"] = latlist
     d["temprature"] = templist
     d["visibility"] = vislist
     d["precipitation"] = preciplist
     d["wind speed"] = windlist
-=======
+    return d
+
 def sort_elev_top_ten(avg_elev):
     top_sort=sorted(avg_elev.items(),key=lambda t: t[1],reverse=True)
     return top_sort[:10]
->>>>>>> 45d036be9d20bfc3baf1ddc2ad70aa1ca0c7cf8e
 
 def barchart_avg_elev_by_state(top_elev_dict):
     x=[]
@@ -69,8 +70,15 @@ def barchart_avg_elev_by_state(top_elev_dict):
 if __name__ == "__main__":
     conn=sqlite3.connect('db_fin_new.sqlite')
     cur=conn.cursor()
-    calc_avg_elev(conn,cur)
     elev_dict=calc_avg_elev(conn,cur)
     top_elev_dict=sort_elev_top_ten(elev_dict)
-    #write_calculation("calc.json",elev_dict)
+    write_calculation("calc.json",elev_dict)
+
+    map_dict = avg_temp(conn,cur)
+    z = {}
+    z["elevation"] = elev_dict
+    z["map data"] = map_dict
+    write_calculation("calc.json",z)
+
     barchart_avg_elev_by_state(top_elev_dict)
+

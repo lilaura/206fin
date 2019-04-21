cedb89e97df3e1892417080261b991fc43345038
Ariel Huang <arielhuang@0587344821.wireless.umich.net>
2019-04-21 15:10:12 -0400

    Calculate avg elevation for airports in each state

--- vis2.py
+++ vis2.py
@@ -8,6 +8,18 @@ import requests
 import time
 
 
+def calc_avg_elev(conn, cur,):
+    cur.execute('SELECT state, elevation FROM Airports ORDER BY state')
+    d={}
+    for row in cur:
+        state=row[0]
+        elevation=row[1]
+        d[state]=d.get(state,[]).append(elevation)
+    for key in d:
+        d[key]=sum(d[key])/len(d[key])
+    return d
+
+def 
 
 
 
